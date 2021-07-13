731752140b6b6d49a2f652ad4f52abdaf2c27675
lj61321 <85833945+lj61321@users.noreply.github.com>
2021-07-13 14:48:19 -0400

    Add files via upload

---
+++ MultilevelInheritance/src/MultiLevelInheritanceDemo.java
@@ -0,0 +1,24 @@
+
+class A{
+	public A(){
+		System.out.println("Class A");
+	}
+}
+
+class B extends A{
+	public B(){
+		System.out.println("Class B");
+	}
+}
+
+class C extends B{
+	public C() {
+		System.out.println("Class C");
+	}
+}
+public class MultiLevelInheritanceDemo {
+	public static void main(String[] args) {
+		C c = new C();
+		
+	}
+}
